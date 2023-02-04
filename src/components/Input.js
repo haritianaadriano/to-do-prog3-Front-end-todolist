4ae21286efb82cd23b002f0bf8c1af9114af521c
haritiana adriano <hei.adriano.4@gmail.com>
2023-02-04 18:26:59 +0300

    finish: divise todo app to multiple component

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,6 +3,7 @@ export default function Input({input, setInput}){
         <div>
             <input 
                 value={input} 
+                placeholder="Make a task"
                 onChange={e => setInput(e.target.value)}
             />
         </div>
