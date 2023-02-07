9ab94274850e962317a840ff5a86b3110039ff6b
haritiana adriano <hei.adriano.4@gmail.com>
2023-02-07 18:03:51 +0300

    feat: input snapshot test

--- src/__test__/input.test.js
+++ src/__test__/input.test.js
@@ -1,8 +1,19 @@
 import {render, cleanup, screen} from "@testing-library/react";
+import {create} from 'react-test-renderer';
 import Input from "../components/Input";
 
-test("", ()=>{
-    render(<Input/>);
-    const inputElement = screen.getByTestId("input-id");
-    expect(inputElement).toBeInTheDocument();
+describe("Input test", () => {
+    it("Input appears in document", () => {
+        render(<Input/>);
+        const inputElement = screen.getByTestId("input-id");
+
+        expect(inputElement).toBeInTheDocument();
+    })
+
+    it("[snapshot] Input renders correctly", () => {
+        const renderer = create(<Input/>);
+        const json = renderer.toJSON();
+
+        expect(json).toMatchSnapshot();
+    })
 })
