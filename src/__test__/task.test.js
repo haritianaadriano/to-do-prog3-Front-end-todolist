be27547a47786630d07d1e6f8938e26ebe153b01
haritiana adriano <hei.adriano.4@gmail.com>
2023-02-07 17:11:11 +0300

    refactor: task.test.js

--- src/__test__/task.test.js
+++ src/__test__/task.test.js
@@ -2,9 +2,15 @@ import {render, cleanup, screen} from "@testing-library/react";
 import Task from "../components/Task";
 
 describe("Task test", () => {
+    const todoMock = "todo-1";
 
     it("Task appears in document", () => {
         const taskElement = screen.getByTestId("task-id");
         expect(taskElement).toBeInTheDocument();
+    })
+
+    it("todo mocking appears in task component", () => {
+        const taskElement = render(<Task todo = {todoMock}/>);
+        expect(taskElement.container).toHaveTextContent(todoMock);
     })
 })
